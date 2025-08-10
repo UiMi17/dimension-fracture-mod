bd91fcb40288bec9e2773dcf361e506d19680230
YuuReigetsu <reigetsuyuu@gmail.com>
2025-08-10 12:00:01 -0300

    ADDED: Creative mode tab

--- src/main/java/xyz/hungryit/rpg/dragons/dimensionfracture/block/ModBlocks.java
+++ src/main/java/xyz/hungryit/rpg/dragons/dimensionfracture/block/ModBlocks.java
@@ -6,8 +6,10 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraftforge.common.crafting.conditions.FalseCondition;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -22,7 +24,7 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, DimensionFracture.MOD_ID);
 
     public static final RegistryObject<Block> ANCIENT_ALTAR = registerBlock("ancient_altar",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).sound(SoundType.STONE)));
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
     public static final RegistryObject<Block> NETHER_REALITY_SHARD_ORE = registerBlock("nether_reality_shard_ore",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.NETHER_GOLD_ORE).sound(SoundType.NETHER_GOLD_ORE)));
     public static final RegistryObject<Block> END_STONE_REALITY_SHARD_ORE = registerBlock("end_stone_reality_shard_ore",
